34c6e5134b1b446bc21e85d9a8ef24465412a9b9
SiriusSagittarius <svenhaaseausmuch@gmail.com>
2026-09-10 11:49:20 +0200

    Restrict PostThrottle rate limiting to POST requests only

--- forum_app/throttling.py
+++ forum_app/throttling.py
@@ -3,3 +3,9 @@ from rest_framework.throttling import UserRateThrottle
 
 class PostThrottle(UserRateThrottle):
     scope = 'posts'
+
+    def allow_request(self, request, view):
+        if request.method != 'POST':
+            return True
+
+        return super().allow_request(request, view)
