6178b6b324dddccac6beb8924ef2beef3b06f785
SiriusSagittarius <svenhaaseausmuch@gmail.com>
2026-09-10 11:46:41 +0200

    Add DEFAULT_THROTTLE_CLASSES to REST_FRAMEWORK settings

--- core/settings.py
+++ core/settings.py
@@ -60,6 +60,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ],
 }
 
 ROOT_URLCONF = 'core.urls'
